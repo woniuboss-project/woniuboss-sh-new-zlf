c5b2448f63b9eb6e175b15b2218c9636cc53ff90
lifei <17710414056@163.com>
2020-04-25 16:58:12 +0800

    add:报表中心_教学部/就业部接口测试

--- WoniuBoss4.0/接口测试代码整理/woniuboss4.0_python/testcase/test_report_market.py
+++ WoniuBoss4.0/接口测试代码整理/woniuboss4.0_python/testcase/test_report_market.py
@@ -8,16 +8,17 @@ from util.utility import Utility
 data_config_path = '../config/fei_testdata.conf'
 data_config_info = Utility.get_json(data_config_path)
 
-query_all_data = Utility.get_excel_to_tuple(data_config_info[0])
-
+query_market = Utility.get_excel_to_tuple(data_config_info[0])
+query_class = Utility.get_excel_to_tuple(data_config_info[1])
+query_jobInfo = Utility.get_excel_to_tuple(data_config_info[2])
 class ReportMarkTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         cls.obj = ReportMarketBank()
 
-    @parameterized.expand(query_all_data)
-    def test_query_question_all(self,url,method,test_data,resp_code,resp_content):
+    @parameterized.expand(query_market)
+    def test_query_market(self,url,method,test_data,resp_code,resp_content):
         resp = self.obj.query_all(url,test_data)
         content = resp.json()
         print(content[len(content)-1]["num"])
@@ -27,5 +28,26 @@ class ReportMarkTest(unittest.TestCase):
             result = "querry_fail"
         self.assertEqual(resp_content,result)
 
+    @parameterized.expand(query_class)
+    def test_query_class(self, url, method, test_data, resp_code, resp_content):
+        resp = self.obj.query_all(url, test_data)
+        content = resp.json()
+        print(content)
+        if content["totalRow"] == 2:
+            result = "querry_success"
+        else:
+            result = "querry_fail"
+        self.assertEqual(resp_content, result)
+
+    @parameterized.expand(query_jobInfo)
+    def test_query_class(self, url, method, test_data, resp_code, resp_content):
+        resp = self.obj.query_all(url, test_data)
+        content = resp.json()
+        print(content)
+        if content["totalRow"] == 1:
+            result = "querry_success"
+        else:
+            result = "querry_fail"
+        self.assertEqual(resp_content, result)
 if __name__ == '__main__':
     unittest.main(verbosity=2)
